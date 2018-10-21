43f88a065e802ff9e67b491636e1d9d96b7c1045
HannaBorodina <hanna.borodina23@gmail.com>
2018-10-21 15:59:19 +0300

    Added new class

---
+++ TaskHandler/TaskHandler/TaskSequHandler.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Concurrent;
+
+namespace TaskHandler
+{
+    public class TaskSequHandler
+    {
+        private static TaskSequHandler _instance;
+        private static object @lock = new object();
+        private ConcurrentQueue<Action> _tasksToHandle = new ConcurrentQueue<Action>();
+
+        public void CheckQueue()
+        {
+            lock (@lock)
+            {
+                if (_tasksToHandle.IsEmpty)
+                    return;
+
+                while (!_tasksToHandle.IsEmpty)
+                {
+                    if (_tasksToHandle.TryDequeue(out Action @task))
+                        @task();
+                }
+            }
+        }
+
+        public static TaskSequHandler GetInstance()
+        {
+            if (_instance == null)
+            {
+                lock (@lock)
+                {
+                    if (_instance == null)
+                        _instance = new TaskSequHandler();
+                }
+            }
+            return _instance;
+        }
+
+        public void AddForExecution(Action taskToHandle)
+        {
+            _tasksToHandle.Enqueue(taskToHandle);
+            CheckQueue();
+        }
+
+    }
+
+}
---
+++ TaskHandler/Tests/TaskHandlerTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+using TaskHandler;
+using Xunit;
+
+namespace Tests
+{
+    public class TaskHandlerTest
+    {
+        [Fact]
+        public void TestForTaskHandler()
+        {
+            var handler = TaskSequHandler.GetInstance();
+
+            handler.AddForExecution(() => Console.WriteLine("Test1"));
+            handler.AddForExecution(() => {
+                Thread.Sleep(2000);
+                Console.WriteLine("Test2");
+            });
+            handler.AddForExecution(() => Console.WriteLine("Test3"));
+        }
+       
+    }
+}
